f582e46a2ec6ec41396ae36ae7215eb7a3769036
Lee1997 <leelaffan97@gmail.com>
2017-06-03 12:46:20 +0100

    Problem 7 completed

--- Q1to9/Problem4.java
+++ Q1to9/Problem4.java
@@ -6,7 +6,8 @@ https://projecteuler.net/problem=4
 
 package solutions.Q1to9;
 
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Problem4 {
 	public static void main(String[] args) {
@@ -14,14 +15,17 @@ public class Problem4 {
 	}
 	
 	public static int findNumber(){
-		return IntStream.range(100, 999)
-				        .filter(e -> isPalindrome(e))
-				        .reduce((first, second) -> second)
-				        .getAsInt();
+		return (Stream.iterate(999, e -> e - 1)
+				     .limit(899)
+			         .filter(i -> Stream.iterate(999, e2 -> e2 - 1)
+			        		            .limit(899)			        		           
+			        		            .anyMatch(j -> isPalindrome(i * j))
+			        		            )).getAsInt();
+				     
 	}
 	
-	public static boolean isPalindrome(int e){
-		String s = Integer.toString(e);
+	public static boolean isPalindrome(Object e){
+		String s = Integer.toString((int) e);
 		return s.equals(new StringBuffer(s).reverse().toString().trim());	  
 	}
 }
